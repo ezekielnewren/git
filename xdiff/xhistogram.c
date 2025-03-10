3392a94f4d06e58550f6eb04beba77bd01b4c259
Ezekiel Newren <ezekielnewren@gmail.com>
2025-03-09 20:11:15 -0600

    xdiff: import xhistogram.c from rustify 2

--- xdiff/xhistogram.c
+++ xdiff/xhistogram.c
@@ -1,193 +1,148 @@
-/*
- * Copyright (C) 2010, Google Inc.
- * and other copyright owners as documented in JGit's IP log.
- *
- * This program and the accompanying materials are made available
- * under the terms of the Eclipse Distribution License v1.0 which
- * accompanies this distribution, is reproduced below, and is
- * available at http://www.eclipse.org/org/documents/edl-v10.php
- *
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or
- * without modification, are permitted provided that the following
- * conditions are met:
- *
- * - Redistributions of source code must retain the above copyright
- *   notice, this list of conditions and the following disclaimer.
- *
- * - Redistributions in binary form must reproduce the above
- *   copyright notice, this list of conditions and the following
- *   disclaimer in the documentation and/or other materials provided
- *   with the distribution.
- *
- * - Neither the name of the Eclipse Foundation, Inc. nor the
- *   names of its contributors may be used to endorse or promote
- *   products derived from this software without specific prior
- *   written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
- * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
- * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
- * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 
 #include "xinclude.h"
+#include "ivec.h"
 
-#define MAX_PTR	UINT_MAX
-#define MAX_CNT	UINT_MAX
+#define MAX_CHAIN_LENGTH 64
 
-#define LINE_END(n) (line##n + count##n - 1)
-#define LINE_END_PTR(n) (*line##n + *count##n - 1)
+struct record {
+	usize ptr, cnt;
+	usize next;
+};
+
+DEFINE_IVEC_TYPE(struct record, record);
 
 struct histindex {
-	struct record {
-		unsigned int ptr, cnt;
-		struct record *next;
-	} **records, /* an occurrence */
-	  **line_map; /* map of line to record chain */
-	chastore_t rcha;
-	unsigned int *next_ptrs;
-	unsigned int table_bits,
-		     records_size,
-		     line_map_size;
-
-	unsigned int max_chain_length,
-		     key_shift,
-		     ptr_shift;
-
-	unsigned int cnt,
-		     has_common;
-
-	xdfenv_t *env;
-	xpparam_t const *xpp;
+	ivec_record record_storage;
+	ivec_usize record_chain;
+	ivec_usize line_map;
+	ivec_usize next_ptrs;
+	usize ptr_shift;
+	usize cnt;
+	bool has_common;
 };
 
 struct region {
-	unsigned int begin1, end1;
-	unsigned int begin2, end2;
+	usize begin1, end1;
+	usize begin2, end2;
 };
 
-#define LINE_MAP(i, a) (i->line_map[(a) - i->ptr_shift])
+static bool mph_equal_by_line_number(xdfenv_t *env, usize lhs, usize rhs) {
+	u64 mph1 = env->xdf1.minimal_perfect_hash->ptr[lhs - LINE_SHIFT];
+	u64 mph2 = env->xdf2.minimal_perfect_hash->ptr[rhs - LINE_SHIFT];
+	return mph1 == mph2;
+}
 
-#define NEXT_PTR(index, ptr) \
-	(index->next_ptrs[(ptr) - index->ptr_shift])
+static i32 scanA(struct histindex *index, xdfenv_t *env, usize start1, usize end1) {
+	usize tbl_idx;
+	usize chain_len;
+	usize rec_cur_idx, rec_new_idx;
+	struct record *rec_cur;
+	struct record rec_new;
 
-#define CNT(index, ptr) \
-	((LINE_MAP(index, ptr))->cnt)
-
-#define MPH(env, s, l) \
-	(env->xdf##s.minimal_perfect_hash->ptr[l - 1])
-
-#define CMP(i, s1, l1, s2, l2) \
-	(MPH(i->env, s1, l1) == MPH(i->env, s2, l2))
-
-#define TABLE_HASH(index, side, line) \
-	XDL_HASHLONG((MPH(index->env, side, line)), index->table_bits)
-
-static int scanA(struct histindex *index, int line1, int count1)
-{
-	unsigned int ptr, tbl_idx;
-	unsigned int chain_len;
-	struct record **rec_chain, *rec;
-
-	for (ptr = LINE_END(1); line1 <= ptr; ptr--) {
-		tbl_idx = TABLE_HASH(index, 1, ptr);
-		rec_chain = index->records + tbl_idx;
-		rec = *rec_chain;
+	for (usize i = end1; i > start1; i -= 1) {
+		bool continue_scan = false;
+		usize ptr = i - 1;
+		tbl_idx = env->xdf1.minimal_perfect_hash->ptr[ptr - LINE_SHIFT];
+		rec_cur_idx = index->record_chain.ptr[tbl_idx];
 
 		chain_len = 0;
-		while (rec) {
-			if (CMP(index, 1, rec->ptr, 1, ptr)) {
+		while (rec_cur_idx != INVALID_INDEX) {
+			u64 mph1, mph2;
+			rec_cur = &index->record_storage.ptr[rec_cur_idx];
+			mph1 = env->xdf1.minimal_perfect_hash->ptr[rec_cur->ptr - LINE_SHIFT];
+			mph2 = env->xdf1.minimal_perfect_hash->ptr[ptr - LINE_SHIFT];
+			if (mph1 == mph2) {
 				/*
 				 * ptr is identical to another element. Insert
 				 * it onto the front of the existing element
 				 * chain.
 				 */
-				NEXT_PTR(index, ptr) = rec->ptr;
-				rec->ptr = ptr;
-				/* cap rec->cnt at MAX_CNT */
-				rec->cnt = XDL_MIN(MAX_CNT, rec->cnt + 1);
-				LINE_MAP(index, ptr) = rec;
-				goto continue_scan;
+				index->next_ptrs.ptr[ptr - index->ptr_shift] = rec_cur->ptr;
+				rec_cur->ptr = ptr;
+				rec_cur->cnt = rec_cur->cnt + 1;
+				index->line_map.ptr[ptr - index->ptr_shift] = rec_cur_idx;
+				continue_scan = true;
+				break;
 			}
 
-			rec = rec->next;
+			rec_cur_idx = rec_cur->next;
 			chain_len++;
 		}
 
-		if (chain_len == index->max_chain_length)
+		if (continue_scan)
+			continue;
+
+		if (chain_len == MAX_CHAIN_LENGTH)
 			return -1;
 
 		/*
 		 * This is the first time we have ever seen this particular
 		 * element in the sequence. Construct a new chain for it.
 		 */
-		if (!(rec = xdl_cha_alloc(&index->rcha)))
-			return -1;
-		rec->ptr = ptr;
-		rec->cnt = 1;
-		rec->next = *rec_chain;
-		*rec_chain = rec;
-		LINE_MAP(index, ptr) = rec;
-
-continue_scan:
-		; /* no op */
+		rec_new_idx = index->record_storage.length;
+		rec_new.ptr = ptr;
+		rec_new.cnt = 1;
+		rec_new.next = index->record_chain.ptr[tbl_idx];
+		rust_ivec_push(&index->record_storage, &rec_new);
+		index->record_chain.ptr[tbl_idx] = rec_new_idx;
+		index->line_map.ptr[ptr - index->ptr_shift] = rec_new_idx;
 	}
 
 	return 0;
 }
 
-static int try_lcs(struct histindex *index, struct region *lcs, int b_ptr,
-	int line1, int count1, int line2, int count2)
+static usize try_lcs(struct histindex *index, xdfenv_t *env, struct region *lcs, usize b_ptr,
+	usize start1, usize end1, usize start2, usize end2)
 {
-	unsigned int b_next = b_ptr + 1;
-	struct record *rec = index->records[TABLE_HASH(index, 2, b_ptr)];
-	unsigned int as, ae, bs, be, np, rc;
-	int should_break;
+	struct record *rec_cur;
+	usize b_next = b_ptr + 1;
+	usize tbl_idx = env->xdf2.minimal_perfect_hash->ptr[b_ptr - LINE_SHIFT];
+	usize as, ae, bs, be, np, rc;
+	bool should_break;
 
-	for (; rec; rec = rec->next) {
-		if (rec->cnt > index->cnt) {
+	for (usize rec_cur_idx = index->record_chain.ptr[tbl_idx];
+		rec_cur_idx != INVALID_INDEX; rec_cur_idx = rec_cur->next) {
+		rec_cur = &index->record_storage.ptr[rec_cur_idx];
+		if (rec_cur->cnt > index->cnt) {
 			if (!index->has_common)
-				index->has_common = CMP(index, 1, rec->ptr, 2, b_ptr);
+				index->has_common = mph_equal_by_line_number(env, rec_cur->ptr, b_ptr);
 			continue;
 		}
 
-		as = rec->ptr;
-		if (!CMP(index, 1, as, 2, b_ptr))
+		as = rec_cur->ptr;
+		if (!mph_equal_by_line_number(env, as, b_ptr))
 			continue;
 
-		index->has_common = 1;
+		index->has_common = true;
 		for (;;) {
-			should_break = 0;
-			np = NEXT_PTR(index, as);
+			should_break = false;
+			np = index->next_ptrs.ptr[as - index->ptr_shift];
 			bs = b_ptr;
 			ae = as;
 			be = bs;
-			rc = rec->cnt;
+			rc = rec_cur->cnt;
 
-			while (line1 < as && line2 < bs
-				&& CMP(index, 1, as - 1, 2, bs - 1)) {
+			while (start1 < as && start2 < bs
+				&& mph_equal_by_line_number(env, as - 1, bs - 1)) {
 				as--;
 				bs--;
-				if (1 < rc)
-					rc = XDL_MIN(rc, CNT(index, as));
+				if (1 < rc) {
+					usize rec_t_idx = index->line_map.ptr[as - index->ptr_shift];
+					struct record *rec_t = &index->record_storage.ptr[rec_t_idx];
+					usize cnt = rec_t->cnt;
+					rc = XDL_MIN(rc, cnt);
+				}
 			}
-			while (ae < LINE_END(1) && be < LINE_END(2)
-				&& CMP(index, 1, ae + 1, 2, be + 1)) {
+			while (ae + 1 < end1 && be + 1 < end2
+				&& mph_equal_by_line_number(env, ae + 1, be + 1)) {
 				ae++;
 				be++;
-				if (1 < rc)
-					rc = XDL_MIN(rc, CNT(index, ae));
+				if (1 < rc) {
+					usize rec_t_idx = index->line_map.ptr[ae - index->ptr_shift];
+					struct record *rec_t = &index->record_storage.ptr[rec_t_idx];
+					usize cnt = rec_t->cnt;
+					rc = XDL_MIN(rc, cnt);
+				}
 			}
 
 			if (b_next <= be)
@@ -200,12 +155,12 @@ static int try_lcs(struct histindex *index, struct region *lcs, int b_ptr,
 				index->cnt = rc;
 			}
 
-			if (np == 0)
+			if (np == 0 || np == INVALID_INDEX)
 				break;
 
 			while (np <= ae) {
-				np = NEXT_PTR(index, np);
-				if (np == 0) {
+				np = index->next_ptrs.ptr[np - index->ptr_shift];
+				if (np == 0 || np == INVALID_INDEX) {
 					should_break = 1;
 					break;
 				}
@@ -220,8 +175,8 @@ static int try_lcs(struct histindex *index, struct region *lcs, int b_ptr,
 	return b_next;
 }
 
-static int fall_back_to_classic_diff(xpparam_t const *xpp, xdfenv_t *env,
-		int line1, int count1, int line2, int count2)
+static i32 fall_back_to_classic_diff(xpparam_t const *xpp, xdfenv_t *env,
+		usize line1, usize count1, usize line2, usize count2)
 {
 	xpparam_t xpparam;
 
@@ -232,135 +187,123 @@ static int fall_back_to_classic_diff(xpparam_t const *xpp, xdfenv_t *env,
 				  line1, count1, line2, count2);
 }
 
-static inline void free_index(struct histindex *index)
-{
-	xdl_free(index->records);
-	xdl_free(index->line_map);
-	xdl_free(index->next_ptrs);
-	xdl_cha_free(&index->rcha);
+static inline void free_index(struct histindex *index) {
+	rust_ivec_free(&index->record_storage);
+	rust_ivec_free(&index->record_chain);
+	rust_ivec_free(&index->line_map);
+	rust_ivec_free(&index->next_ptrs);
 }
 
-static int find_lcs(xpparam_t const *xpp, xdfenv_t *env,
+static i32 find_lcs(xdfenv_t *env,
 		    struct region *lcs,
-		    int line1, int count1, int line2, int count2)
+		    usize start1, usize end1, usize start2, usize end2)
 {
-	int b_ptr;
-	int ret = -1;
+	i32 ret = -1;
 	struct histindex index;
-
+	usize table_size = env->xdf1.record->length;
+	usize default_value = INVALID_INDEX;
 	memset(&index, 0, sizeof(index));
 
-	index.env = env;
-	index.xpp = xpp;
+	IVEC_INIT(index.record_storage);
+	IVEC_INIT(index.record_chain);
+	IVEC_INIT(index.line_map);
+	IVEC_INIT(index.next_ptrs);
 
-	index.records = NULL;
-	index.line_map = NULL;
-	/* in case of early xdl_cha_free() */
-	index.rcha.head = NULL;
+	rust_ivec_resize(&index.record_chain, env->minimal_perfect_hash_size, &default_value);
+	rust_ivec_resize(&index.line_map, table_size, &default_value);
+	rust_ivec_resize(&index.next_ptrs, table_size, &default_value);
 
-	index.table_bits = xdl_hashbits(count1);
-	index.records_size = 1 << index.table_bits;
-	if (!XDL_CALLOC_ARRAY(index.records, index.records_size))
-		goto cleanup;
+	index.ptr_shift = start1;
 
-	index.line_map_size = count1;
-	if (!XDL_CALLOC_ARRAY(index.line_map, index.line_map_size))
-		goto cleanup;
+	if (scanA(&index, env, start1, end1)) {
+		free_index(&index);
+		return ret;
+	}
 
-	if (!XDL_CALLOC_ARRAY(index.next_ptrs, index.line_map_size))
-		goto cleanup;
+	index.cnt = MAX_CHAIN_LENGTH + 1;
 
-	/* lines / 4 + 1 comes from xprepare.c:xdl_prepare_ctx() */
-	if (xdl_cha_init(&index.rcha, sizeof(struct record), count1 / 4 + 1) < 0)
-		goto cleanup;
+	for (usize b_ptr = start2; b_ptr + 1 <= end2; )
+		b_ptr = try_lcs(&index, env, lcs, b_ptr, start1, end1, start2, end2);
 
-	index.ptr_shift = line1;
-	index.max_chain_length = 64;
-
-	if (scanA(&index, line1, count1))
-		goto cleanup;
-
-	index.cnt = index.max_chain_length + 1;
-
-	for (b_ptr = line2; b_ptr <= LINE_END(2); )
-		b_ptr = try_lcs(&index, lcs, b_ptr, line1, count1, line2, count2);
-
-	if (index.has_common && index.max_chain_length < index.cnt)
+	if (index.has_common && MAX_CHAIN_LENGTH < index.cnt)
 		ret = 1;
 	else
 		ret = 0;
 
-cleanup:
 	free_index(&index);
 	return ret;
 }
 
 static int histogram_diff(xpparam_t const *xpp, xdfenv_t *env,
-	int line1, int count1, int line2, int count2)
+	usize start1, usize end1, usize start2, usize end2)
 {
 	struct region lcs;
-	int lcs_found;
-	int result;
-redo:
-	result = -1;
+	i32 lcs_found;
+	i32 result;
 
-	if (count1 <= 0 && count2 <= 0)
-		return 0;
+	while (true) {
+		result = -1;
 
-	if (LINE_END(1) >= MAX_PTR)
-		return -1;
+		if (start1 >= end1 && start2 >= end2)
+			return 0;
 
-	if (!count1) {
-		while(count2--)
-			env->xdf2.consider.ptr[SENTINEL + line2++ - 1] = YES;
-		return 0;
-	} else if (!count2) {
-		while(count1--)
-			env->xdf1.consider.ptr[SENTINEL + line1++ - 1] = YES;
-		return 0;
-	}
-
-	memset(&lcs, 0, sizeof(lcs));
-	lcs_found = find_lcs(xpp, env, &lcs, line1, count1, line2, count2);
-	if (lcs_found < 0)
-		goto out;
-	else if (lcs_found)
-		result = fall_back_to_classic_diff(xpp, env, line1, count1, line2, count2);
-	else {
-		if (lcs.begin1 == 0 && lcs.begin2 == 0) {
-			while (count1--)
-				env->xdf1.consider.ptr[SENTINEL + line1++ - 1] = YES;
-			while (count2--)
-				env->xdf2.consider.ptr[SENTINEL + line2++ - 1] = YES;
-			result = 0;
-		} else {
-			result = histogram_diff(xpp, env,
-						line1, lcs.begin1 - line1,
-						line2, lcs.begin2 - line2);
-			if (result)
-				goto out;
-			/*
-			 * result = histogram_diff(xpp, env,
-			 *            lcs.end1 + 1, LINE_END(1) - lcs.end1,
-			 *            lcs.end2 + 1, LINE_END(2) - lcs.end2);
-			 * but let's optimize tail recursion ourself:
-			*/
-			count1 = LINE_END(1) - lcs.end1;
-			line1 = lcs.end1 + 1;
-			count2 = LINE_END(2) - lcs.end2;
-			line2 = lcs.end2 + 1;
-			goto redo;
+		if (start1 == end1) {
+			for (; start2 < end2; start2 += 1) {
+				env->xdf2.consider.ptr[SENTINEL + start2 - 1] = YES;
+			}
+			return 0;
 		}
+		if (start2 == end2) {
+			for (; start1 < end1; start1 += 1) {
+				env->xdf1.consider.ptr[SENTINEL + start1 - 1] = YES;
+			}
+			return 0;
+		}
+
+		memset(&lcs, 0, sizeof(lcs));
+		lcs_found = find_lcs(env, &lcs, start1, end1, start2, end2);
+		if (lcs_found < 0)
+			return result;
+		else if (lcs_found)
+			result = fall_back_to_classic_diff(xpp, env, start1, end1 - start1, start2, end2 - start1);
+		else {
+			if (lcs.begin1 == 0 && lcs.begin2 == 0) {
+				for (; start1 < end1; start1 += 1) {
+					env->xdf1.consider.ptr[SENTINEL + start1 - 1] = YES;
+				}
+				for (; start2 < end2; start2 += 1) {
+					env->xdf2.consider.ptr[SENTINEL + start2 - 1] = YES;
+				}
+				result = 0;
+			} else {
+				result = histogram_diff(xpp, env,
+							start1, lcs.begin1,
+							start2, lcs.begin2);
+				if (result)
+					return result;
+				/*
+				 * result = histogram_diff(xpp, env,
+				 *            lcs.end1 + 1, end1,
+				 *            lcs.end2 + 1, end2);
+				 * but let's optimize tail recursion ourself:
+				*/
+				start1 = lcs.end1 + 1;
+				start2 = lcs.end2 + 1;
+
+				continue;
+			}
+		}
+		break;
 	}
-out:
 	return result;
 }
 
+
 int xdl_do_histogram_diff(xpparam_t const *xpp, xdfenv_t *env) {
-	isize end1 = env->xdf1.record->length - env->delta_end;
-	isize end2 = env->xdf2.record->length - env->delta_end;
+	isize end1 = env->xdf1.record->length;
+	isize end2 = env->xdf2.record->length;
 
 	return histogram_diff(xpp, env,
-		env->delta_start + LINE_SHIFT, end1 - env->delta_start,
-		env->delta_start + LINE_SHIFT, end2 - env->delta_start);
+		env->delta_start + 1, end1 + LINE_SHIFT,
+		env->delta_start + 1, end2 + LINE_SHIFT);
 }
