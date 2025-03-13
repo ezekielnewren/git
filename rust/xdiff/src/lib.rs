1a08c2770862401fe5de1a0c3ece4a0a16b71cab
Ezekiel Newren <ezekielnewren@gmail.com>
2025-03-12 20:02:03 -0600

    xdiff: process lines serially

--- rust/xdiff/src/lib.rs
+++ rust/xdiff/src/lib.rs
@@ -10,7 +10,7 @@ unsafe extern "C" fn xdl_hash_records(file: *mut xdfile, flags: u64) {
     let file: &mut xdfile = &mut *file;
 
     let slice: &mut [xrecord] = file.record.as_mut_slice();
-    slice.par_iter_mut().for_each(|rec| {
+    slice.iter_mut().for_each(|rec| {
         rec.line_hash = xxh3_64(rec.as_ref());
     });
 
