e0d12372b26024a6a0e378ca7fea683344fec910
Ezekiel Newren <ezekielnewren@gmail.com>
2025-03-14 08:09:25 -0600

    xdiff: create ChainMap and test it

--- rust/xdiff/src/maps.rs
+++ rust/xdiff/src/maps.rs
@@ -120,7 +120,7 @@ enum ProbeResult {
     OutOfMemory,
 }
 
-struct Entry<K, V> {
+struct FixedMapEntry<K, V> {
     key: K,
     value: V,
 }
@@ -128,7 +128,7 @@ struct Entry<K, V> {
 
 pub struct FixedMap<'a, K, V, HE: HashEq<K>> {
     meta: &'a mut [u64],
-    data: &'a mut [Entry<K, V>],
+    data: &'a mut [FixedMapEntry<K, V>],
     meta_layout: Layout,
     data_layout: Layout,
     size: usize,
@@ -161,14 +161,14 @@ impl<'a, K, V, HE: HashEq<K>> FixedMap<'a, K, V, HE> {
     {
         let po2 = (capacity*2).next_power_of_two();
         let meta_layout = Layout::array::<u64>(po2).unwrap();
-        let data_layout = Layout::array::<Entry<K, V>>(po2).unwrap();
+        let data_layout = Layout::array::<FixedMapEntry<K, V>>(po2).unwrap();
 
         let ptr1 = unsafe { std::alloc::alloc_zeroed(meta_layout) };
         let ptr2 = unsafe { std::alloc::alloc(data_layout) };
 
         Self {
             meta: unsafe { std::slice::from_raw_parts_mut(ptr1 as *mut u64, po2) },
-            data: unsafe { std::slice::from_raw_parts_mut(ptr2 as *mut Entry<K, V>, po2) },
+            data: unsafe { std::slice::from_raw_parts_mut(ptr2 as *mut FixedMapEntry<K, V>, po2) },
             meta_layout,
             data_layout,
             size: 0,
@@ -201,7 +201,7 @@ impl<'a, K, V, HE: HashEq<K>> FixedMap<'a, K, V, HE> {
     fn _overwrite(&mut self, index: usize, hash: u64, key: K, value: V) {
         self.meta[index] = hash;
         unsafe {
-            std::ptr::write(&mut self.data[index], Entry {
+            std::ptr::write(&mut self.data[index], FixedMapEntry {
                 key,
                 value,
             });
@@ -283,6 +283,173 @@ impl<'a, K, V, HE: HashEq<K>> FixedMap<'a, K, V, HE> {
 
 
 
+struct ChainMapEntry<K, V> {
+    key_hash: u64,
+    key: K,
+    value: V,
+    next: *mut ChainMapEntry<K, V>,
+}
+
+pub struct ChainMap<'a, K, V, HE: HashEq<K>> {
+    head: &'a mut [*mut ChainMapEntry<K, V>],
+    entry: &'a mut [ChainMapEntry<K, V>],
+    head_layout: Layout,
+    entry_layout: Layout,
+    count: usize,
+    capacity: usize,
+    mask: usize,
+    he: HE,
+}
+
+
+impl<'a, K, V, HE: HashEq<K>> Drop for ChainMap<'a, K, V, HE> {
+    fn drop(&mut self) {
+        unsafe {
+            if std::mem::needs_drop::<V>() {
+                for i in 0..self.head.len() {
+                    let mut cur = self.head[i];
+                    while !cur.is_null() {
+                        let kv = &mut *cur;
+                        std::ptr::drop_in_place(&mut kv.value);
+                        cur = kv.next;
+                    }
+                }
+            }
+
+            std::alloc::dealloc(self.head.as_mut_ptr() as *mut u8, self.head_layout);
+            std::alloc::dealloc(self.entry.as_mut_ptr() as *mut u8, self.entry_layout);
+        }
+    }
+}
+
+
+impl<'a, K: Hash + Eq, V> ChainMap<'a, K, V, DefaultHashEq<K>> {
+    pub fn with_capacity(capacity: usize) -> Self {
+        Self::with_capacity_and_hash_eq(capacity, DefaultHashEq::new())
+    }
+}
+
+
+impl<'a, K, V, HE: HashEq<K>> ChainMap<'a, K, V, HE> {
+
+    pub fn with_capacity_and_hash_eq(capacity: usize, inst: HE) -> Self
+    {
+        let po2 = capacity.next_power_of_two();
+        let head_layout = Layout::array::<*mut ChainMapEntry<K, V>>(po2).unwrap();
+        let entry_layout = Layout::array::<ChainMapEntry<K, V>>(po2).unwrap();
+
+        let ptr1 = unsafe { std::alloc::alloc_zeroed(head_layout) };
+        let ptr2 = unsafe { std::alloc::alloc(entry_layout) };
+
+        Self {
+            head: unsafe { std::slice::from_raw_parts_mut(ptr1 as *mut *mut ChainMapEntry<K, V>, po2) },
+            entry: unsafe { std::slice::from_raw_parts_mut(ptr2 as *mut ChainMapEntry<K, V>, po2) },
+            head_layout,
+            entry_layout,
+            count: 0,
+            capacity,
+            mask: po2 - 1,
+            he: inst,
+        }
+    }
+
+
+    fn _push(&mut self, key: K, hash: u64, value: V)  -> *mut ChainMapEntry<K, V> {
+        let i = hash as usize & self.mask;
+        let dst = &mut self.entry[self.count];
+        unsafe {
+            std::ptr::write(dst, ChainMapEntry {
+                key_hash: hash,
+                key,
+                value,
+                next: self.head[i],
+            });
+        }
+        self.head[i] = dst;
+        self.count += 1;
+        dst
+    }
+
+    fn _find_entry(&self, key: &K, hash: u64) -> *mut ChainMapEntry<K, V> {
+        let mut cur = self.head[hash as usize & self.mask];
+        while !cur.is_null() {
+            let entry = unsafe { &mut *cur };
+            if entry.key_hash == hash && self.he.eq(&entry.key, key) {
+                return cur;
+            }
+            cur = entry.next;
+        }
+
+        std::ptr::null_mut()
+    }
+
+    pub fn contains_key(&self, key: &K) -> bool {
+        let hash = self.he.hash(key);
+        let entry = self._find_entry(key, hash);
+        !entry.is_null()
+    }
+
+    pub fn get(&self, key: &K) -> Option<&V> {
+        let hash = self.he.hash(key);
+        let entry = self._find_entry(key, hash);
+        if !entry.is_null() {
+            Some(unsafe { &(*entry).value })
+        } else {
+            None
+        }
+    }
+
+    pub fn get_or_insert(&mut self, key: &K, value: V) -> &mut V
+    where K: Clone
+    {
+        let hash = self.he.hash(key);
+        let entry = self._find_entry(key, hash);
+        if !entry.is_null() {
+            unsafe { &mut (*entry).value }
+        } else {
+            unsafe { &mut (*self._push(key.clone(), hash, value)).value }
+        }
+    }
+
+    pub fn get_or_default(&mut self, key: &K) -> &mut V
+    where K: Clone, V: Default
+    {
+        let hash = self.he.hash(key);
+        let entry = self._find_entry(key, hash);
+        if !entry.is_null() {
+            unsafe { &mut (*entry).value }
+        } else {
+            unsafe { &mut (*self._push(key.clone(), hash, V::default())).value }
+        }
+    }
+
+    pub fn insert(&mut self, key: K, value: V) {
+        let hash = self.he.hash(&key);
+        let entry = self._find_entry(&key, hash);
+        if !entry.is_null() {
+            unsafe { (*entry).value = value };
+        } else {
+            self._push(key, hash, value);
+        }
+    }
+
+    pub fn capacity(&self) -> usize {
+        self.capacity
+    }
+    pub fn len(&self) -> usize {
+        self.count
+    }
+
+}
+
+
+
+
+
+
+
+
+
 
 #[cfg(test)]
 mod tests {
@@ -291,7 +458,7 @@ mod tests {
     use std::io::BufRead;
     use std::path::PathBuf;
     use crate::mock::helper::read_test_file;
-    use crate::maps::{DefaultHashEq, HashEq, HashEqHasher, FixedMap};
+    use crate::maps::{DefaultHashEq, HashEq, HashEqHasher, FixedMap, ChainMap};
     use crate::xtypes::{xrecord, xrecord_he};
 
     const FURNITURE: [&str; 41] = [
@@ -348,7 +515,7 @@ mod tests {
     }
 
     #[test]
-    fn test_new() {
+    fn test_new_fixed_map() {
         let flags = 0u64;
 
         let mut list_vec: Vec<Vec<String>> = Vec::new();
@@ -367,6 +534,41 @@ mod tests {
             let mut monotonic = 0u64;
 
             let mut fm = FixedMap::with_capacity(list.len());
+            for key in list.iter() {
+                let expected = (key.clone(), mphb_simple.hash(&key));
+                let mph = *fm.get_or_insert(key, monotonic);
+                if mph == monotonic {
+                    monotonic += 1;
+                }
+                let actual = (key.clone(), mph);
+                assert_eq!(expected, actual);
+            }
+
+            let mph_size = fm.len();
+            assert_eq!(mphb_simple.map.len(), mph_size);
+        }
+    }
+
+    #[test]
+    fn test_chain_map() {
+        let flags = 0u64;
+
+        let mut list_vec: Vec<Vec<String>> = Vec::new();
+        list_vec.push(FRUIT.iter().map(|s| s.to_string()).collect());
+        list_vec.push(FURNITURE.iter().map(|s| s.to_string()).collect());
+        let data = read_test_file(&PathBuf::from("xhistogram/gitdump.txt")).unwrap();
+        let dump: Vec<String> = data.lines().map(|v| v.unwrap()).collect();
+        list_vec.push(dump);
+
+        for list in list_vec {
+            let mut mphb_simple = MPHBSimple {
+                map: HashMap::new(),
+                monotonic: 0,
+            };
+
+            let mut monotonic = 0u64;
+
+            let mut fm = ChainMap::with_capacity(list.len());
             for key in list.iter() {
                 let expected = (key.clone(), mphb_simple.hash(&key));
                 let mph = *fm.get_or_insert(key, monotonic);
