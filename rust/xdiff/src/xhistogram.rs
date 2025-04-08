5be91c60f44a4f0beac1dd451e8cc05ad5fea01f
Ezekiel Newren <ezekielnewren@gmail.com>
2025-04-07 18:19:04 -0600

    xdiff: delete xdl_find_lcs()

--- rust/xdiff/src/xhistogram.rs
+++ rust/xdiff/src/xhistogram.rs
@@ -1,8 +1,8 @@
 #![allow(non_camel_case_types)]
 
 use std::marker::PhantomData;
+use std::ops::Range;
 use interop::ivec::IVec;
-use crate::get_file_context;
 use crate::xdiff::{LINE_SHIFT, SENTINEL, YES};
 use crate::xdiffi::classic_diff_with_range;
 use crate::xtypes::{xdpair, FileContext};
@@ -71,17 +71,15 @@ struct histindex {
 
 #[repr(C)]
 struct region {
-	begin1: usize,
-	end1: usize,
-	begin2: usize,
-	end2: usize,
+	range1: Range<usize>,
+	range2: Range<usize>,
 }
 
 
-fn scan_a(index: &mut histindex, pair: &mut xdpair, line1: usize, count1: usize) -> i32 {
+fn scan_a(index: &mut histindex, pair: &mut xdpair, range1: Range<usize>) -> i32 {
     let lhs = FileContext::new(&mut pair.lhs);
 
-    for ptr in (line1..=line1 + count1 - 1).rev() {
+    for ptr in range1.rev() {
 		let mut continue_scan = false;
 		let tbl_idx = lhs.minimal_perfect_hash[ptr - LINE_SHIFT] as usize;
 
@@ -143,19 +141,18 @@ fn record_equal(pair: &xdpair, i1: usize, i2: usize) -> bool {
 
 
 unsafe fn try_lcs(index: &mut histindex, pair: &mut xdpair, lcs: &mut region, b_ptr: usize,
-	line1: usize, count1: usize, line2: usize, count2: usize
+	range1: Range<usize>, range2: Range<usize>,
 ) -> usize {
 	let rhs = FileContext::new(&mut pair.rhs);
 
 	let mut b_next = b_ptr + 1;
 	let tbl_idx = rhs.minimal_perfect_hash[b_ptr - LINE_SHIFT] as usize;
-	let mut range_a = 0..0;
-	let mut range_b = 0..0;
+	let mut range_a = Range::default();
+	let mut range_b = Range::default();
 	let mut np;
 	let mut rc;
 	let mut should_break;
 
-	// for (; rec; rec = rec->next) {
 	for rec in RecordIter::new(index.record[tbl_idx]) {
 		if (*rec).cnt > index.cnt {
 			if !index.has_common {
@@ -178,7 +175,7 @@ unsafe fn try_lcs(index: &mut histindex, pair: &mut xdpair, lcs: &mut region, b_
 			range_b.end = range_b.start;
 			rc = (*rec).cnt;
 
-			while line1 < range_a.start && line2 < range_b.start
+			while range1.start < range_a.start && range2.start < range_b.start
 				&& record_equal(pair, range_a.start - 1, range_b.start - 1) {
 				range_a.start -= 1;
 				range_b.start -= 1;
@@ -188,7 +185,7 @@ unsafe fn try_lcs(index: &mut histindex, pair: &mut xdpair, lcs: &mut region, b_
 					rc = std::cmp::min(rc, cnt);
 				}
 			}
-			while range_a.end < line1 + count1 - 1 && range_b.end < line2 + count2 - 1
+			while range_a.end < range1.end - 1 && range_b.end < range2.end - 1
 				&& record_equal(pair, range_a.end + 1, range_b.end + 1) {
 				range_a.end += 1;
 				range_b.end += 1;
@@ -202,11 +199,11 @@ unsafe fn try_lcs(index: &mut histindex, pair: &mut xdpair, lcs: &mut region, b_
 			if b_next <= range_b.end {
 				b_next = range_b.end + 1;
 			}
-			if lcs.end1 - lcs.begin1 < range_a.end - range_a.start || rc < index.cnt {
-				lcs.begin1 = range_a.start;
-				lcs.begin2 = range_b.start;
-				lcs.end1 = range_a.end;
-				lcs.end2 = range_b.end;
+			if lcs.range1.end - lcs.range1.start < range_a.end - range_a.start || rc < index.cnt {
+				lcs.range1.start = range_a.start;
+				lcs.range2.start = range_b.start;
+				lcs.range1.end = range_a.end;
+				lcs.range2.end = range_b.end;
 				index.cnt = rc;
 			}
 
@@ -234,40 +231,30 @@ unsafe fn try_lcs(index: &mut histindex, pair: &mut xdpair, lcs: &mut region, b_
 }
 
 
-#[no_mangle]
-unsafe extern "C" fn xdl_find_lcs(pair: *mut xdpair, lcs: *mut region,
-				   line1: usize, count1: usize, line2: usize, count2: usize
-) -> i32 {
-	let pair = xdpair::from_raw_mut(pair);
-	let lcs = &mut *lcs;
-
-	find_lcs(pair, lcs, line1, count1, line2, count2)
-}
-
 unsafe fn find_lcs(pair: &mut xdpair, lcs: &mut region,
-	line1: usize, count1: usize, line2: usize, count2: usize
+	range1: Range<usize>, range2: Range<usize>,
 ) -> i32 {
 	let fudge = ((*pair.lhs.record).len() + (*pair.rhs.record).len()) * 10;
 
 	let mut index = histindex {
 		record_storage: IVec::with_capacity(fudge),
 		record: IVec::zero(pair.minimal_perfect_hash_size),
-		line_map: IVec::zero(count1),
-		next_ptrs: IVec::zero(count1),
-		ptr_shift: line1,
+		line_map: IVec::zero(range1.len()),
+		next_ptrs: IVec::zero(range1.len()),
+		ptr_shift: range1.start,
 		cnt: 0,
 		has_common: false,
 	};
 
-	if scan_a(&mut index, pair, line1, count1) != 0 {
+	if scan_a(&mut index, pair, range1.clone()) != 0 {
 		return -1;
 	}
 
 	index.cnt = MAX_CHAIN_LENGTH + 1;
 
-	let mut b_ptr = line2;
-	while b_ptr <= line2 + count2 - 1 {
-		b_ptr = try_lcs(&mut index, pair, lcs, b_ptr, line1, count1, line2, count2);
+	let mut b_ptr = range2.start;
+	while b_ptr < range2.end {
+		b_ptr = try_lcs(&mut index, pair, lcs, b_ptr, range1.clone(), range2.clone());
 	}
 
 	if index.has_common && MAX_CHAIN_LENGTH < index.cnt {
@@ -280,7 +267,7 @@ unsafe fn find_lcs(pair: &mut xdpair, lcs: &mut region,
 
 #[no_mangle]
 unsafe extern "C" fn histogram_diff(flags: u64, pair: *mut xdpair,
-	mut line1: usize, mut count1: usize, mut line2: usize, mut count2: usize
+	mut range1: Range<usize>, mut range2: Range<usize>,
 ) -> i32 {
 	let pair = xdpair::from_raw_mut(pair);
 
@@ -288,58 +275,48 @@ unsafe extern "C" fn histogram_diff(flags: u64, pair: *mut xdpair,
 	loop {
 		result = -1;
 
-		if count1 <= 0 && count2 <= 0 {
+		if range1.len() <= 0 && range2.len() <= 0 {
 			return 0;
 		}
 
-		if count1 == 0 {
-			while count2 > 0 {
-				count2 -= 1;
-				pair.rhs.consider[SENTINEL + line2 - LINE_SHIFT] = YES;
-				line2 += 1;
+		if range1.len() == 0 {
+			for i in range2 {
+				pair.rhs.consider[SENTINEL + i - LINE_SHIFT] = YES;
 			}
 			return 0;
 		}
-		if count2 == 0 {
-			while count1 > 0 {
-				count1 -= 1;
-				pair.lhs.consider[SENTINEL + line1 - LINE_SHIFT] = YES;
-				line1 += 1;
+		if range2.len() == 0 {
+			for i in range1 {
+				pair.lhs.consider[SENTINEL + i - LINE_SHIFT] = YES;
 			}
 			return 0;
 		}
 
 		let mut lcs = region {
-			begin1: 0,
-			end1: 0,
-			begin2: 0,
-			end2: 0,
+			range1: Range::default(),
+			range2: Range::default(),
 		};
-		let lcs_found = xdl_find_lcs(pair, &mut lcs, line1, count1, line2, count2);
+		let lcs_found = find_lcs(pair, &mut lcs, range1.clone(), range2.clone());
 		if lcs_found < 0 {
 			return result;
 		}
 
 		if lcs_found != 0 {
-			return classic_diff_with_range(flags, pair, line1..line1 + count1, line2..line2 + count2);
+			return classic_diff_with_range(flags, pair, range1, range2);
 		}
 
-		if lcs.begin1 == 0 && lcs.begin2 == 0 {
-			while count1 > 0 {
-				count1 -= 1;
-				pair.lhs.consider[SENTINEL + line1 - 1] = YES;
-				line1 += 1;
+		if lcs.range1.start == 0 && lcs.range2.start == 0 {
+			for i in range1 {
+				pair.lhs.consider[SENTINEL + i - 1] = YES;
 			}
-			while count2 > 0 {
-				count2 -= 1;
-				pair.rhs.consider[SENTINEL + line2 - 1] = YES;
-				line2 += 1;
+			for i in range2 {
+				pair.rhs.consider[SENTINEL + i - 1] = YES;
 			}
 			result = 0;
 		} else {
 			result = histogram_diff(flags, pair,
-						line1, lcs.begin1 - line1,
-						line2, lcs.begin2 - line2);
+						range1.start..lcs.range1.start,
+						range2.start..lcs.range2.start);
 			if result != 0 {
 				return result;
 			}
@@ -349,10 +326,8 @@ unsafe extern "C" fn histogram_diff(flags: u64, pair: *mut xdpair,
 			 *            lcs.end2 + 1, LINE_END(2) - lcs.end2);
 			 * but let's optimize tail recursion ourself:
 			*/
-			count1 = line1 + count1 - 1 - lcs.end1;
-			line1 = lcs.end1 + 1;
-			count2 = line2 + count2 - 1 - lcs.end2;
-			line2 = lcs.end2 + 1;
+			range1.start = lcs.range1.end + 1;
+			range2.start = lcs.range2.end + 1;
 			continue;
 		}
 		break;
@@ -366,11 +341,14 @@ unsafe extern "C" fn histogram_diff(flags: u64, pair: *mut xdpair,
 unsafe extern "C" fn xdl_do_histogram_diff(flags: u64, pair: *mut xdpair) -> i32 {
 	let pair = xdpair::from_raw_mut(pair);
 
-	let end1 = (*pair.lhs.record).len() - pair.delta_end;
-	let end2 = (*pair.rhs.record).len() - pair.delta_end;
+	let mut range1 = Range::default();
+	let mut range2 = Range::default();
 
-	histogram_diff(flags, pair,
-		LINE_SHIFT + pair.delta_start, LINE_SHIFT + end1 - 1 - pair.delta_start,
-		LINE_SHIFT + pair.delta_start, LINE_SHIFT + end2 - 1 - pair.delta_start)
+	range1.start = LINE_SHIFT + pair.delta_start;
+	range1.end = LINE_SHIFT + (*pair.lhs.record).len() - pair.delta_end;
+	range2.start = LINE_SHIFT + pair.delta_start;
+	range2.end = LINE_SHIFT + (*pair.rhs.record).len() - pair.delta_end;
+
+	histogram_diff(flags, pair, range1, range2)
 }
 
