8c9d42b7c2348812f4bf6097f3c3326ebf520adc
Ezekiel Newren <ezekielnewren@gmail.com>
2025-04-21 15:26:44 -0600

    xdiff: make xdl_append_merge() more rust like

--- xdiff/xmerge.c
+++ xdiff/xmerge.c
@@ -48,22 +48,24 @@ struct xdmerge {
 };
 
 
-static int xdl_append_merge(struct xdmerge **merge, int mode,
-			    long i0, long chg0,
-			    long i1, long chg1,
-			    long i2, long chg2)
+static i32 xdl_append_merge(struct xdmerge **merge, u8 mode,
+			    isize i0, isize chg0,
+			    isize i1, isize chg1,
+			    isize i2, isize chg2)
 {
 	struct xdmerge *m = *merge;
-	if (m && (i1 <= m->i1 + m->chg1 || i2 <= m->i2 + m->chg2)) {
-		if (mode != m->mode)
+	if (m != NULL && (i1 <= m->i1 + m->chg1 || i2 <= m->i2 + m->chg2)) {
+		if (mode != m->mode) {
 			m->mode = 0;
+		}
 		m->chg0 = i0 + chg0 - m->i0;
 		m->chg1 = i1 + chg1 - m->i1;
 		m->chg2 = i2 + chg2 - m->i2;
 	} else {
-		m = xdl_malloc(sizeof(struct xdmerge));
-		if (!m)
+		m = xmalloc(sizeof(struct xdmerge));
+		if (m == NULL) {
 			return -1;
+		}
 		m->next = NULL;
 		m->mode = mode;
 		m->i0 = i0;
@@ -72,8 +74,9 @@ static int xdl_append_merge(struct xdmerge **merge, int mode,
 		m->chg1 = chg1;
 		m->i2 = i2;
 		m->chg2 = chg2;
-		if (*merge)
+		if (*merge != NULL) {
 			(*merge)->next = m;
+		}
 		*merge = m;
 	}
 	return 0;
