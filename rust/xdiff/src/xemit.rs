dd582633c98b8aa6b4b2667f0fdaaca26b4db8fa
Ezekiel Newren <ezekielnewren@gmail.com>
2025-05-05 16:26:57 -0600

    xdiff: implement xdl_get_hunk() in rust

--- rust/xdiff/src/xemit.rs
+++ rust/xdiff/src/xemit.rs
@@ -4,3 +4,62 @@ use crate::xtypes::xdpair;
 
 pub(crate) type emit_func_t = unsafe extern "C" fn(pair: *mut xdpair, xscr: *mut xdchange, ecb: *const xdemitcb,
 			   xecfg: *const xdemitconf) -> i32;
+
+
+/*
+ * Starting at the passed change atom, find the latest change atom to be included
+ * inside the differential hunk according to the specified configuration.
+ * Also advance xscr if the first changes must be discarded.
+ */
+#[no_mangle]
+pub(crate) unsafe extern "C" fn xdl_get_hunk(xscr: *mut *mut xdchange, xecfg: *const xdemitconf) -> *mut xdchange {
+	let max_common = 2 * (*xecfg).ctxlen + (*xecfg).interhunkctxlen;
+	let max_ignorable = (*xecfg).ctxlen;
+	let mut ignored = 0; /* number of ignored blank lines */
+
+	/* remove ignorable changes that are too far before other changes */
+	let mut xchp = *xscr;
+	let mut xch: *mut xdchange = std::ptr::null_mut();
+	while !xchp.is_null() && (*xchp).ignore {
+		xch = (*xchp).next;
+
+		if xch.is_null() || (*xch).i1 - ((*xchp).i1 + (*xchp).chg1) >= max_ignorable {
+			*xscr = xch;
+		}
+		
+		xchp = (*xchp).next;
+	}
+
+	if (*xscr).is_null() {
+		return std::ptr::null_mut();
+	}
+
+	let mut lxch = *xscr;
+	xchp = *xscr;
+	xch = (*xchp).next;
+	while !xch.is_null() {
+		let distance = (*xch).i1 - ((*xchp).i1 + (*xchp).chg1);
+		if distance > max_common {
+			break;
+		}
+
+		if distance < max_ignorable && (!(*xch).ignore || lxch == xchp) {
+			lxch = xch;
+			ignored = 0;
+		} else if distance < max_ignorable && (*xch).ignore {
+			ignored += (*xch).chg2;
+		} else if lxch != xchp && (*xch).i1 + ignored - ((*lxch).i1 + (*lxch).chg1) > max_common {
+			break;
+		} else if !(*xch).ignore {
+			lxch = xch;
+			ignored = 0;
+		} else {
+			ignored += (*xch).chg2;
+		}
+		
+		xchp = xch;
+		xch = (*xch).next;
+	}
+
+	lxch
+}
--- xdiff/xemit.c
+++ xdiff/xemit.c
@@ -44,55 +44,6 @@ static int xdl_emit_record(struct xd_file_context *ctx, long ri, char const *pre
 }
 
 
-/*
- * Starting at the passed change atom, find the latest change atom to be included
- * inside the differential hunk according to the specified configuration.
- * Also advance xscr if the first changes must be discarded.
- */
-struct xdchange *xdl_get_hunk(struct xdchange **xscr, struct xdemitconf const *xecfg)
-{
-	struct xdchange *xch, *xchp, *lxch;
-	long max_common = 2 * xecfg->ctxlen + xecfg->interhunkctxlen;
-	long max_ignorable = xecfg->ctxlen;
-	long ignored = 0; /* number of ignored blank lines */
-
-	/* remove ignorable changes that are too far before other changes */
-	for (xchp = *xscr; xchp && xchp->ignore; xchp = xchp->next) {
-		xch = xchp->next;
-
-		if (xch == NULL ||
-		    xch->i1 - (xchp->i1 + xchp->chg1) >= max_ignorable)
-			*xscr = xch;
-	}
-
-	if (!*xscr)
-		return NULL;
-
-	lxch = *xscr;
-
-	for (xchp = *xscr, xch = xchp->next; xch; xchp = xch, xch = xch->next) {
-		long distance = xch->i1 - (xchp->i1 + xchp->chg1);
-		if (distance > max_common)
-			break;
-
-		if (distance < max_ignorable && (!xch->ignore || lxch == xchp)) {
-			lxch = xch;
-			ignored = 0;
-		} else if (distance < max_ignorable && xch->ignore) {
-			ignored += xch->chg2;
-		} else if (lxch != xchp &&
-			   xch->i1 + ignored - (lxch->i1 + lxch->chg1) > max_common) {
-			break;
-		} else if (!xch->ignore) {
-			lxch = xch;
-			ignored = 0;
-		} else {
-			ignored += xch->chg2;
-		}
-	}
-
-	return lxch;
-}
 
 
 static isize def_ff(u8 const* rec, isize len, u8* buf, isize sz) {
