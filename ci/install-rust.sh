598d651786f5efb76d191830a0f5fabcddece28b
Ezekiel Newren <ezekielnewren@gmail.com>
2025-06-06 17:29:36 -0600

    ensure $CARGO_HOME/env exists

--- ci/install-rust.sh
+++ ci/install-rust.sh
@@ -12,8 +12,12 @@ fi
 
 ## install rustup
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain none -y
+if [ ! -f $CARGO_HOME/env ]; then
+  echo "PATH=$CARGO_HOME/bin:\$PATH" > $CARGO_HOME/env
+fi
+. $CARGO_HOME/env
 ## install a specific version of rust
-$CARGO_HOME/bin/rustup default $RUST_VERSION || exit 1
+rustup default $RUST_VERSION || exit 1
 ## non root user's need write access to $CARGO_HOME to install crates
 chmod 1777 $CARGO_HOME
 
