e5b130b4fdee09800f115a5765854f0f960c83aa
Ezekiel Newren <ezekielnewren@gmail.com>
2026-01-07 18:21:52 -0700

    xdiff/histogram: convert records into an ivec and name it record

--- xdiff/xhistogram.c
+++ xdiff/xhistogram.c
@@ -54,14 +54,14 @@ struct record {
 };
 
 DEFINE_IVEC_TYPE(struct record, record);
+DEFINE_IVEC_TYPE(struct record*, record_ptr);
 
 struct histindex {
-	struct record **records;  /* an occurrence */
-	struct record **line_map; /* map of line to record chain */
+	struct IVec_record_ptr record; /* an occurrence */
+	struct record **line_map;      /* map of line to record chain */
 	struct IVec_record record_storage;
 	size_t *next_ptrs;
 	size_t table_bits,
-	       records_size,
 	       line_map_size;
 
 	size_t ptr_shift;
@@ -102,7 +102,7 @@ static int scanA(struct histindex *index, size_t line1, size_t count1)
 
 	for (ptr = LINE_END(1); line1 <= ptr; ptr--) {
 		tbl_idx = TABLE_HASH(index, 1, ptr);
-		rec_chain = index->records + tbl_idx;
+		rec_chain = &index->record.ptr[tbl_idx];
 		rec = *rec_chain;
 
 		chain_len = 0;
@@ -151,7 +151,7 @@ static int try_lcs(struct histindex *index, struct region *lcs, size_t b_ptr,
 	size_t line1, size_t count1, size_t line2, size_t count2)
 {
 	size_t b_next = b_ptr + 1;
-	struct record *rec = index->records[TABLE_HASH(index, 2, b_ptr)];
+	struct record *rec = index->record.ptr[TABLE_HASH(index, 2, b_ptr)];
 	size_t as, ae, bs, be, np, rc;
 	bool should_break;
 
@@ -222,7 +222,7 @@ static int try_lcs(struct histindex *index, struct region *lcs, size_t b_ptr,
 
 static inline void free_index(struct histindex *index)
 {
-	xdl_free(index->records);
+	ivec_free(&index->record);
 	xdl_free(index->line_map);
 	ivec_free(&index->record_storage);
 	xdl_free(index->next_ptrs);
@@ -231,17 +231,15 @@ static inline void free_index(struct histindex *index)
 static int histindex_init(struct histindex *index, xdfenv_t *env, size_t line1, size_t count1)
 {
 	memset(index, 0, sizeof(struct histindex));
+	IVEC_INIT(index->record);
 	IVEC_INIT(index->record_storage);
 
 	index->env = env;
 
-	index->records = NULL;
 	index->line_map = NULL;
 
 	index->table_bits = xdl_hashbits(count1);
-	index->records_size = 1 << index->table_bits;
-	if (!XDL_CALLOC_ARRAY(index->records, index->records_size))
-		goto abort;
+	ivec_zero(&index->record, 1 << index->table_bits);
 
 	index->line_map_size = count1;
 	if (!XDL_CALLOC_ARRAY(index->line_map, index->line_map_size))
